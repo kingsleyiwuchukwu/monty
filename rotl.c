332a8caedb5c5ce57a020f707d64e6ce90c9ac9b
Kingsley Iwuchukwu <kingsleyiwuchukwu0@gmail.com>
2022-12-19 00:40:30 +0100

    Updated File

--- rotl.c
+++ rotl.c
@@ -11,13 +11,13 @@ void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
     stack_t *tmp = *head, *aux;
     if (*head == NULL || (*head)->next == NULL)
     {
-        return;
+            return;
     }
     aux = (*head)->next;
     aux->prev = NULL;
     while (tmp->next != NULL)
     {
-        tmp = tmp->next;
+            tmp = tmp->next;
     }
     tmp->next = *head;
     (*head)->next = NULL;
